54859a9eb07761b1258ebf3c88e434091099eddc
DanielEstuardo48 <3512949530101@ingenieria.usac.gt>
2024-03-04 19:26:34 -0600

    Se agrega la suma de cambios al modificar un patron

--- main.py
+++ main.py
@@ -7,26 +7,45 @@ class Patron:
         self.S = S
         self.codigo = codigo
         self.patron = patron
-    def editar_nombre(self):
-        self.nombre = input("Ingrese el nuevo nombre: ")
+        self.cambio = 0
 
-    def editar_R(self):
-        self.R = int(input("Ingrese el nuevo valor de R: "))
+    #def editar_nombre(self):
+    #    self.nombre = input("Ingrese el nuevo nombre: ")
 
-    def editar_C(self):
-        self.C = int(input("Ingrese el nuevo valor de C: "))
+    #def editar_R(self):
+    #    self.R = int(input("Ingrese el nuevo valor de R: "))
 
-    def editar_F(self):
-        self.F = int(input("Ingrese el nuevo valor de F: "))
+    #def editar_C(self):
+    #    self.C = int(input("Ingrese el nuevo valor de C: "))
 
-    def editar_S(self):
-        self.S = int(input("Ingrese el nuevo valor de S: "))
+    #def editar_F(self):
+    #    self.F = int(input("Ingrese el nuevo valor de F: "))
 
-    def editar_codigo(self):
-        self.codigo = input("Ingrese el nuevo código: ")
+    #def editar_S(self):
+    #    self.S = int(input("Ingrese el nuevo valor de S: "))
 
-    def editar_patron(self):
-        self.patron = input("Ingrese el nuevo patrón: ")
+    #def editar_codigo(self):
+    #    self.codigo = input("Ingrese el nuevo código de nuevo: ")
+
+    def editar_patron(self, new_patron):
+        self.patron = new_patron
+    
+    def comparar_patron(self, new_patron):
+        if self.patron != new_patron:
+            self.cambio += 1 
+    
+    def editar_patron(self, new_patron):
+        if new_patron != self.patron:
+            x = 0
+            for old, new in zip(self.patron, new_patron):
+                if old != new:
+                    x += 1
+            self.cambio = x
+        self.patron = new_patron
+        #self.patron = new_patron
+        #self.comparar_patron(new_patron)
+        #self.patron = new_patron
+
     def __str__(self):
         return f"{self.nombre} - {self.R} - {self.C} - {self.F} - {self.S} - {self.codigo} - {self.patron}"
 
@@ -79,32 +98,16 @@ class ListaEnlazada:
             print("S: ",seleccion_patron.dato.S)
             print("Codigo: ",seleccion_patron.dato.codigo)
             print("Patron: ",seleccion_patron.dato.patron)
-            print("\nDesea editarlo: ")
-            print("1. Si")
-            print("2. No")
-            opcion = int(input("Seleccione una opcion: "))
 
-            if opcion == 1:
-                seleccion_patron.dato.editar_nombre()
-                seleccion_patron.dato.editar_R()
-                seleccion_patron.dato.editar_C()
-                seleccion_patron.dato.editar_F()
-                seleccion_patron.dato.editar_S()
-                seleccion_patron.dato.editar_codigo()
-                seleccion_patron.dato.editar_patron()
-
-                print("\nPatrón actualizado:")
-                print("Nombre: ", seleccion_patron.dato.nombre)
-                print("R: ", seleccion_patron.dato.R)
-                print("C: ", seleccion_patron.dato.C)
-                print("F: ", seleccion_patron.dato.F)
-                print("S: ", seleccion_patron.dato.S)
-                print("Codigo: ", seleccion_patron.dato.codigo)
-                print("Patron: ", seleccion_patron.dato.patron)
-            elif opcion ==2:
-                menu()
+            new_patron = input("Ingrese el nuevo patrón: ")
+            if len(new_patron) != int(seleccion_patron.dato.R) * int(seleccion_patron.dato.C):
+                    print("\n Error: No se puede editar el patrón, ya que el número de elementos ingresados es diferente al resultado de la multiplicación de R por C.")
             else:
-                print("\nError: Opcion no valida")
+                    seleccion_patron.dato.editar_patron(new_patron)
+
+                    print("\nPatrón actualizado:")
+                    print("Patron: ", seleccion_patron.dato.patron)
+                    print("Cambios: ", seleccion_patron.dato.cambio * seleccion_patron.dato.S)
         else:
             print("Patron no encontrado")
 
@@ -121,10 +124,6 @@ class Patrones:
     def seleccionar_patrones(self):
         codigo = input("\nIngrese el codigo a editar: ")
         lista_enlazada.seleccionar_editar(codigo)
-
-    def editar_patrones(self):
-        print("menu de editar")
-
 
 
 def menu():
