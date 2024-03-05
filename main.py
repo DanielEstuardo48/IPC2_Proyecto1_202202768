9ddb114d5f3ce2ceaae5ea76042ed03a22ff8fd6
DanielEstuardo48 <3512949530101@ingenieria.usac.gt>
2024-03-05 00:03:18 -0600

    Se actualizo la entrada de archivos y el se implemento el contador de pisos cambiados y su precio

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import xml.etree.ElementTree as ET
 class Patron:
     def __init__(self, nombre, R, C, F, S, codigo, patron):
         self.nombre = nombre
@@ -8,31 +10,14 @@ class Patron:
         self.codigo = codigo
         self.patron = patron
         self.cambio = 0
+        self.cambiopos = 0
 
-    #def editar_nombre(self):
-    #    self.nombre = input("Ingrese el nuevo nombre: ")
-
-    #def editar_R(self):
-    #    self.R = int(input("Ingrese el nuevo valor de R: "))
-
-    #def editar_C(self):
-    #    self.C = int(input("Ingrese el nuevo valor de C: "))
-
-    #def editar_F(self):
-    #    self.F = int(input("Ingrese el nuevo valor de F: "))
-
-    #def editar_S(self):
-    #    self.S = int(input("Ingrese el nuevo valor de S: "))
-
-    #def editar_codigo(self):
-    #    self.codigo = input("Ingrese el nuevo código de nuevo: ")
-
-    def editar_patron(self, new_patron):
-        self.patron = new_patron
+    
     
     def comparar_patron(self, new_patron):
         if self.patron != new_patron:
-            self.cambio += 1 
+            self.cambio += 1
+            self.cambiopos = sum(old != new for old, new in zip(self.patron, new_patron)) 
     
     def editar_patron(self, new_patron):
         if new_patron != self.patron:
@@ -42,9 +27,10 @@ class Patron:
                     x += 1
             self.cambio = x
         self.patron = new_patron
-        #self.patron = new_patron
-        #self.comparar_patron(new_patron)
-        #self.patron = new_patron
+        
+        if new_patron != self.patron:
+            self.comparar_patron(new_patron)
+            self.patron = new_patron
 
     def __str__(self):
         return f"{self.nombre} - {self.R} - {self.C} - {self.F} - {self.S} - {self.codigo} - {self.patron}"
@@ -107,16 +93,38 @@ class ListaEnlazada:
 
                     print("\nPatrón actualizado:")
                     print("Patron: ", seleccion_patron.dato.patron)
-                    print("Cambios: ", seleccion_patron.dato.cambio * seleccion_patron.dato.S)
+                    print("Cambios realizados: ", seleccion_patron.dato.cambio)
+                    print("Movimientos realizados: ", seleccion_patron.dato.cambiopos)
+                    print("Precio por mover azulejo: ", seleccion_patron.dato.cambiopos * seleccion_patron.dato.S)
+                    print("Pecio por cambiar azulejo: ", seleccion_patron.dato.cambio * seleccion_patron.dato.F)
         else:
             print("Patron no encontrado")
 
 class Patrones:
     def agregar_patron(self):
-        patron_nuevo = Patron('Ejemplo1', 3, 3, 7, 8, 'S2204', 'BNBNBNBNB')
-        patron_nuevo2 = Patron('Ejemplo2', 2, 2, 7, 8, 'S2205', 'BBNN')
-        lista_enlazada.agregar(patron_nuevo)
-        lista_enlazada.agregar(patron_nuevo2)
+        ruta = "C:/Users/danis/OneDrive/Documents/Quinto Semestre/IPC2/Proyecto/"
+        for file in os.listdir(ruta):
+            if file.endswith('.xml'):
+                ruta = os.path.join(ruta, file)
+                with open(ruta, 'r') as file:
+                    tree = ET.parse(file)
+                    root = tree.getroot()
+                    for piso_element in root.findall('piso'):
+                        nombre = piso_element.get('nombre')
+                        R = int(piso_element.find('R').text)
+                        C = int(piso_element.find('C').text)
+                        F = int(piso_element.find('F').text)
+                        S = int(piso_element.find('S').text)
+
+                        patrones_element = piso_element.find('patrones')
+                        patron_elements = patrones_element.findall('patron')
+
+                        for patron_element in patron_elements:
+                            codigo = patron_element.get('codigo')
+                            patron = patron_element.text
+                            patron_instance = Patron(nombre, R, C, F, S, codigo, patron)
+
+                            lista_enlazada.agregar(patron_instance)
 
     def mostrar_patrones(self):
         lista_enlazada.mostrar()
@@ -154,20 +162,25 @@ def main():
 lista_enlazada = ListaEnlazada()
 main()
 
-# #!ESto iba en agregar
-'''nombre = input("Ingrese el nombre del patron: ")
-        R = int(input("Ingrese el valor de R: "))
-        C = int(input("Ingrese el valor de C: "))
-        F = int(input("Ingrese el valor de F: "))
-        S = int(input("Ingrese el valor de S: "))
-        codigo = input("Ingrese el codigo del patron: ")
-        patron = input("Ingrese el patron: ")'''
 
-# #!Esto estaba en editar patrones
-'''#lista = ListaEnlazada()
-        codigo = input("\nIngrese el codigo a editar: ")
-        seleccion_patron = lista_enlazada.seleccionar(codigo)
-        if seleccion_patron:
-            print(f"Patron seleccionado: {seleccion_patron}")
-        else:
-            print("Patron no encontrado")'''
+#!ESto es para poder editar en clase patron
+#def editar_nombre(self):
+    #    self.nombre = input("Ingrese el nuevo nombre: ")
+
+    #def editar_R(self):
+    #    self.R = int(input("Ingrese el nuevo valor de R: "))
+
+    #def editar_C(self):
+    #    self.C = int(input("Ingrese el nuevo valor de C: "))
+
+    #def editar_F(self):
+    #    self.F = int(input("Ingrese el nuevo valor de F: "))
+
+    #def editar_S(self):
+    #    self.S = int(input("Ingrese el nuevo valor de S: "))
+
+    #def editar_codigo(self):
+    #    self.codigo = input("Ingrese el nuevo código de nuevo: ")
+
+    #def editar_patron(self, new_patron):
+    #    self.patron = new_patron
