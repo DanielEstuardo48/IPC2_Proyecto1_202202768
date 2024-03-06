caa0f46bbb96a994d0f27fd52deac228c4048ee1
DanielEstuardo48 <3512949530101@ingenieria.usac.gt>
2024-03-05 19:34:51 -0600

    Se agrega funcion de comparar precios

--- main.py
+++ main.py
@@ -10,14 +10,12 @@ class Patron:
         self.codigo = codigo
         self.patron = patron
         self.cambio = 0
-        self.cambiopos = 0
+        
 
-    
-    
     def comparar_patron(self, new_patron):
         if self.patron != new_patron:
             self.cambio += 1
-            self.cambiopos = sum(old != new for old, new in zip(self.patron, new_patron)) 
+            #self.cambiopos = sum(old != new for old, new in zip(self.patron, new_patron)) 
     
     def editar_patron(self, new_patron):
         if new_patron != self.patron:
@@ -32,6 +30,25 @@ class Patron:
             self.comparar_patron(new_patron)
             self.patron = new_patron
 
+    def mov_patron(self, new_patron):
+        cambiopos = 0
+        mov_cadena = ''
+        
+        for char in new_patron:
+            if char == "N":
+                mov_cadena += "B"
+                cambiopos += 1
+            elif char == "B":
+                mov_cadena += "N"
+                cambiopos +=1
+            else:
+                if new_patron[-1] == "N":
+                    mov_cadena = mov_cadena[:cambiopos] + char + mov_cadena[cambiopos:]
+                elif mov_cadena[-1] == "B":
+                    mov_cadena = mov_cadena[:cambiopos-1] + char + mov_cadena[cambiopos-1:]
+                cambiopos +=1
+        return mov_cadena, cambiopos
+
     def __str__(self):
         return f"{self.nombre} - {self.R} - {self.C} - {self.F} - {self.S} - {self.codigo} - {self.patron}"
 
@@ -39,6 +56,12 @@ class Nodo:
     def __init__(self, dato):
         self.dato = dato
         self.siguiente = None
+    
+    def __iter__(self):
+        node = self
+        while node:
+            yield node
+            node = node.siguiente
 
 class ListaEnlazada:
     def __init__(self):
@@ -53,18 +76,36 @@ class ListaEnlazada:
                 nodo_actual = nodo_actual.siguiente
             nodo_actual.siguiente = Nodo(dato)
 
-    def mostrar(self):
-        nodo_actual = self.cabeza
-        while nodo_actual:
-            print("\nNombre: ",nodo_actual.dato.nombre)
-            print("R: ",nodo_actual.dato.R)
-            print("C: ",nodo_actual.dato.C)
-            print("F: ",nodo_actual.dato.F)
-            print("S: ",nodo_actual.dato.S)
-            print("Codigo: ",nodo_actual.dato.codigo)
-            print("Patron: ",nodo_actual.dato.patron)
-            nodo_actual = nodo_actual.siguiente
+    def mostrar_odenpornombre(self):
+        sorted_data = sorted(self.cabeza, key=lambda x: x.dato.nombre)
+
+        for nodo in sorted_data:
+            print("\nNombre: ", nodo.dato.nombre)
+            print("R: ", nodo.dato.R)
+            print("C: ", nodo.dato.C)
+            print("F: ", nodo.dato.F)
+            print("S: ", nodo.dato.S)
+            print("Codigo: ", nodo.dato.codigo)
+            print("Patron: ", nodo.dato.patron)
+            #print("Cambio: ", nodo.dato.cambio)
+            #print("Cambio pos: ", nodo.dato.cambiopos)
+            print("-------------------------")
     
+    def mostrar_odenporcodigo(self):
+        sorted_data = sorted(self.cabeza, key=lambda x: x.dato.codigo)
+
+        for nodo in sorted_data:
+            print("\nNombre: ", nodo.dato.nombre)
+            print("R: ", nodo.dato.R)
+            print("C: ", nodo.dato.C)
+            print("F: ", nodo.dato.F)
+            print("S: ", nodo.dato.S)
+            print("Codigo: ", nodo.dato.codigo)
+            print("Patron: ", nodo.dato.patron)
+            #print("Cambio: ", nodo.dato.cambio)
+            #print("Cambio pos: ", nodo.dato.cambiopos)
+            print("-------------------------")
+
     def buscar(self, codigo):
         actual = self.cabeza
         while actual:
@@ -85,18 +126,27 @@ class ListaEnlazada:
             print("Codigo: ",seleccion_patron.dato.codigo)
             print("Patron: ",seleccion_patron.dato.patron)
 
-            new_patron = input("Ingrese el nuevo patrón: ")
+            new_patron = input("Ingrese el nuevo patrón: ").upper()
             if len(new_patron) != int(seleccion_patron.dato.R) * int(seleccion_patron.dato.C):
                     print("\n Error: No se puede editar el patrón, ya que el número de elementos ingresados es diferente al resultado de la multiplicación de R por C.")
             else:
                     seleccion_patron.dato.editar_patron(new_patron)
+                    mov_cadena, cambiopos = seleccion_patron.dato.mov_patron(new_patron)
+
+                    #seleccion_patron.dato.mov_patron(new_patron)
 
                     print("\nPatrón actualizado:")
                     print("Patron: ", seleccion_patron.dato.patron)
                     print("Cambios realizados: ", seleccion_patron.dato.cambio)
-                    print("Movimientos realizados: ", seleccion_patron.dato.cambiopos)
-                    print("Precio por mover azulejo: ", seleccion_patron.dato.cambiopos * seleccion_patron.dato.S)
-                    print("Pecio por cambiar azulejo: ", seleccion_patron.dato.cambio * seleccion_patron.dato.F)
+                    print("Movimientos realizados: ", cambiopos)
+                    print("Precio por mover azulejo: Q", cambiopos * seleccion_patron.dato.S)
+                    print("Pecio por cambiar azulejo: Q", seleccion_patron.dato.cambio * seleccion_patron.dato.F)
+                    if cambiopos * seleccion_patron.dato.S < seleccion_patron.dato.cambio * seleccion_patron.dato.F:
+                        print("\n======== Sugerencia ==========")
+                        print("El mejor precio a escoger es el de mover azulejo: Q", cambiopos * seleccion_patron.dato.S)
+                    elif cambiopos * seleccion_patron.dato.S > seleccion_patron.dato.cambio * seleccion_patron.dato.F:
+                        print("\n======== Sugerencia ==========")
+                        print("El mejor precio a escoger es el de cambiar azulejo: Q", seleccion_patron.dato.cambio * seleccion_patron.dato.F)
         else:
             print("Patron no encontrado")
 
@@ -127,7 +177,18 @@ class Patrones:
                             lista_enlazada.agregar(patron_instance)
 
     def mostrar_patrones(self):
-        lista_enlazada.mostrar()
+        print("\n======================  Tipo de Orden ===================")
+        print("1. Orden alfabético de pisos")
+        print("2. Orden alfabético de patrón por piso")
+        opcion = int(input("Ingrese una opcion: "))
+
+        if opcion == 1:
+            lista_enlazada.mostrar_odenpornombre()
+        elif opcion == 2:
+            lista_enlazada.mostrar_odenporcodigo()
+        else:
+            print("\nError: Opcion no valida")
+        
     
     def seleccionar_patrones(self):
         codigo = input("\nIngrese el codigo a editar: ")
@@ -138,7 +199,7 @@ def menu():
     print("\n ================= Menu Inicial =====================")
     print("1. Agregar archivo XML con patrones")
     print("2. Mostrar patrones")
-    print("3. Editar patron")
+    print("3. Modificar patron")
     print("4. Salir")
     opcion = int(input("Seleccione una opción: "))
     return opcion
@@ -161,26 +222,3 @@ def main():
 
 lista_enlazada = ListaEnlazada()
 main()
-
-
-#!ESto es para poder editar en clase patron
-#def editar_nombre(self):
-    #    self.nombre = input("Ingrese el nuevo nombre: ")
-
-    #def editar_R(self):
-    #    self.R = int(input("Ingrese el nuevo valor de R: "))
-
-    #def editar_C(self):
-    #    self.C = int(input("Ingrese el nuevo valor de C: "))
-
-    #def editar_F(self):
-    #    self.F = int(input("Ingrese el nuevo valor de F: "))
-
-    #def editar_S(self):
-    #    self.S = int(input("Ingrese el nuevo valor de S: "))
-
-    #def editar_codigo(self):
-    #    self.codigo = input("Ingrese el nuevo código de nuevo: ")
-
-    #def editar_patron(self, new_patron):
-    #    self.patron = new_patron
